c11a3ceadbe4e093c4a3f590f95a3a026b0d60de
Houssam Manaouch <61112686+houssam-manaouch@users.noreply.github.com>
2026-08-28 00:05:04 +0200

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for(let i = 0; i < nums.length; i++){
+            let diff = target - nums[i]
+            let diffIndex = nums.indexOf(diff, i + 1)
+            
+
+            if(diffIndex !== -1){
+                return [i, diffIndex]
+            }
+
+        }
+
+
+    }
+}
